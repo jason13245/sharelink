bc6698c10ff18f1993c1de4925627130e1a3471a
jason13245 <jason13245@gmail.com>
2018-01-03 14:59:50 +0800

    adding list model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { list } from './link/link.model';
 
 @Component({
   selector: 'app-root',
@@ -21,11 +22,12 @@ export class AppComponent implements OnInit{
     title:string,
     tag:string
   }){
-    this.links.push({
-      url: formData.url,
-      title:formData.title,
-      tag:formData.tag
-    });
+    this.links.push(new list(formData.url,formData.title,formData.tag));
+    // this.links.push({
+    //   url: formData.url,
+    //   title:formData.title,
+    //   tag:formData.tag
+    // });
     localStorage.setItem('storedList',JSON.stringify(this.links));
     this.linksCounter =this.links.length;
     localStorage.setItem('listCounter',JSON.stringify(this.linksCounter));
---
+++ src/app/link/link.model.ts
@@ -0,0 +1,7 @@
+export class list{
+    constructor(public url:string,public title:string,public tag:string){
+        this.url=url;
+        this.title=title;
+        this.tag=tag;
+    }
+}
